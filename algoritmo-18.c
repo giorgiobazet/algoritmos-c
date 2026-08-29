4a267656bc7a74d9629c0f7c47e536d92061c16f
Giorgio Bazet <giorgiocsb@gmail.com>
2026-08-29 00:14:03 -0300

    Adicionando novo algoritmo

---
+++ algoritmo-18.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+     
+    int x, y, z;
+
+    printf("Entre com o primeiro valor inteiro: ");
+    scanf("%d", &x);
+
+    printf("\nEntre com o segundo valor inteiro: ");
+    scanf("%d", &y);
+
+    printf("\nEntre com o terceiro valor inteiro: ");
+    scanf("%d", &z);
+
+    if ((y > x) && (z > x))
+        printf("\nO menor valor é: %d\n", x);
+    else if ((x > y) && (z > y))
+        printf("\nO menor valor é: %d\n", y);
+    else
+        printf("\nO menor valor é: %d\n", z);
+
+    return 0;
+
+}
